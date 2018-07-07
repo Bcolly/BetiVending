ecdc2b71674d47da92f3ab535a7123657f180c54
Oier Beaskoetxea <obeaskoetxea@gmail.com>
2018-07-07 14:18:06 +0200

    lista de maquinas y gestion de rutas 2

--- users/methods/cambiarRuta.php
+++ users/methods/cambiarRuta.php
@@ -4,7 +4,7 @@ require_once ("../../conexion.php");
 
 if (isset($_GET['mid'])&&isset($_GET['rid'])){
 	$basededatos = conectardb();
-	execute("UPDATE v_maquinas SET idlocal = 0 WHERE id=$_GET[id]", $basededatos, 1);
+	execute("UPDATE v_maquinas SET idlocal = 0 WHERE id=$_GET[mid]", $basededatos, 1);
 	execute("UPDATE v_dispositivo SET idlocal = 0 WHERE id=(SELECT id FROM v_maquinas WHERE id=$_GET[id])", $basededatos, 2);
 }
 ?>
